50999cb65a7118a9c5b9755dd039fbef15c72321
Salman <salman_khan1993@hotmail.com>
2018-01-16 02:08:50 +1100

    Marker changed from sydney to User's Current Location

--- app/src/main/java/com/mesalmankhan/userscurrentlocationkotlin/MapsActivity.kt
+++ app/src/main/java/com/mesalmankhan/userscurrentlocationkotlin/MapsActivity.kt
@@ -52,11 +52,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
+
+        updateLocationUI()
+        getDeviceLocation()
+
+        if(mLocationPermissionGranted){
+            // Add a marker in Sydney and move the camera
+            val imHere = LatLng(mLastKnownLocation!!.latitude, mLastKnownLocation!!.longitude)
+            mMap.addMarker(MarkerOptions().position(imHere).title("I'm Here"))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(imHere))
+        }
 
 
 
